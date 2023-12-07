aa7efccb3a63cb3ef5142963872cddf7495dbab7
Bryan Costa <bryancmoraes776@gmail.com>
2023-12-06 22:20:58 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- scope/src/main/java/com/AulaJpaMaven2/scope/config/TestConfig.java
+++ scope/src/main/java/com/AulaJpaMaven2/scope/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.AulaJpaMaven2.scope.entities.Category;
 import com.AulaJpaMaven2.scope.entities.Order;
+import com.AulaJpaMaven2.scope.entities.OrderItem;
 import com.AulaJpaMaven2.scope.entities.Product;
 import com.AulaJpaMaven2.scope.entities.User;
 import com.AulaJpaMaven2.scope.entities.enums.OrderStatus;
 import com.AulaJpaMaven2.scope.repositories.CategoryRepository;
+import com.AulaJpaMaven2.scope.repositories.OrderItemRepository;
 import com.AulaJpaMaven2.scope.repositories.OrderRepository;
 import com.AulaJpaMaven2.scope.repositories.ProductRepository;
 import com.AulaJpaMaven2.scope.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 	
 	
@@ -75,5 +80,12 @@ public class TestConfig implements CommandLineRunner {
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4 ));
 	}
 }
